8ef7af0ef527ec1b2c6547dfbebe97f4203f4cf0
leoh7 <leoh7@naver.com>
2019-05-01 02:13:36 +0900

    [DI] 의존성 제공: 메인 객체는 두 의존성 반환값을 합해 반환한다

--- src/DI/DiContainer_01_tests.js
+++ src/DI/DiContainer_01_tests.js
@@ -42,5 +42,33 @@ describe('DiContainer', function() {
       });
       expect(container.get(name)).toBe(returnFromRegisteredFunction);
     });
+
+    it('등록된 함수에 의존성을 제공한다', function() {
+      var main = 'main',
+          mainFunc,
+          dep1 = 'dep1',
+          dep2 = 'dep2';
+      
+      container.register(main, [dep1, dep2], function(dep1Func, dep2Func) {
+        return function() {
+          return dep1Func() + dep2Func();
+        };
+      });
+
+      container.register(dep1, [], function() {
+        return function() {
+          return 1;
+        };
+      });
+
+      container.register(dep2, [], function() {
+        return function() {
+          return 2;
+        };
+      });
+
+      mainFunc = container.get(main);
+      expect(mainFunc()).toBe(3);
+    });
   });
 });
