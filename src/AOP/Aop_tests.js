f0fc00427ce35148000cb15fe6d0b719e65e52d6
leoh7 <leoh7@naver.com>
2019-05-09 08:08:11 +0900

    [AOP] TDD: AOP expect를 작성하고 에러를 확인

--- src/AOP/Aop_tests.js
+++ src/AOP/Aop_tests.js
@@ -1,7 +1,7 @@
 describe('Aop', () => {
   describe('Aop.around(fnName, advice, targetObj)', () => {
     it('타깃 함수를 호출 시, 어드바이스를 실행하도록 한다', () => {
-      
+      expect(excutedAdvice).toBe(true);
     });
   });
   
