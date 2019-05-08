ac08f1df787c1ac3c8cf7a0c6ebd7febde1bc235
leoh7 <leoh7@naver.com>
2019-05-09 08:09:13 +0900

    [AOP] TDD: 간단하게 에러 상황 회피

--- src/AOP/Aop_tests.js
+++ src/AOP/Aop_tests.js
@@ -1,6 +1,12 @@
 describe('Aop', () => {
   describe('Aop.around(fnName, advice, targetObj)', () => {
     it('타깃 함수를 호출 시, 어드바이스를 실행하도록 한다', () => {
+      // 테스트의 신호등 역할
+      var excutedAdvice = false;
+      var advice = function() {
+        excutedAdvice = true;
+      };
+      advice();
       expect(excutedAdvice).toBe(true);
     });
   });
