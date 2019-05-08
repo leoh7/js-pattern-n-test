c0dc3181fffa17a3deaf6ded3d92b14bc55c208c
leoh7 <leoh7@naver.com>
2019-05-09 08:31:43 +0900

    [AOP] TDD: 구현부를 층에 맞게 리팩토링, 최종적으로 이 단계에서 커버리지를 가진 제품 코드를 추출해 낼 것임

--- src/AOP/Aop_tests.js
+++ src/AOP/Aop_tests.js
@@ -1,4 +1,9 @@
 describe('Aop', () => {
+  Aop = {
+    around: function(fnName, advice, fnObj) {
+      fnObj[fnName] = advice;
+    }
+  }
   describe('Aop.around(fnName, advice, targetObj)', () => {
     it('타깃 함수를 호출 시, 어드바이스를 실행하도록 한다', () => {
       var targetObj = {
@@ -9,11 +14,6 @@ describe('Aop', () => {
       var advice = function() {
         excutedAdvice = true;
       };
-      Aop = {
-        around: function(fnName, advice, targetObj) {
-          advice();
-        }
-      }
       Aop.around('targetFn', advice, targetObj);
       targetObj.targetFn();
       expect(excutedAdvice).toBe(true);
