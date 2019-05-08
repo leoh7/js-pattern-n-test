27626f2c551f350eedc9cc74ce9921b769f9e851
leoh7 <leoh7@naver.com>
2019-05-09 08:16:32 +0900

    [AOP] TDD: 타깃함수 실행을 가로채는 around 함수를 가정

--- src/AOP/Aop_tests.js
+++ src/AOP/Aop_tests.js
@@ -1,12 +1,16 @@
 describe('Aop', () => {
   describe('Aop.around(fnName, advice, targetObj)', () => {
     it('타깃 함수를 호출 시, 어드바이스를 실행하도록 한다', () => {
+      var targetObj = {
+        targetFn: function () {}
+      }
       // 테스트의 신호등 역할
       var excutedAdvice = false;
       var advice = function() {
         excutedAdvice = true;
       };
-      advice();
+      Aop.around('targetFn', advice, targetObj);
+      targetObj.targetFn();
       expect(excutedAdvice).toBe(true);
     });
   });
