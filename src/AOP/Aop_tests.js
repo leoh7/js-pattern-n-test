041e82231a5512a5b8e1fa0b2bdfc09334bdd060
leoh7 <leoh7@naver.com>
2019-05-09 19:08:10 +0900

    [AOP] TDD: 팩토리를 생성해 테스트 통과

--- src/AOP/Aop_tests.js
+++ src/AOP/Aop_tests.js
@@ -44,6 +44,17 @@ describe('Aop', () => {
       );
     });
     it('마지막 어드바이스가 기존 어드바이스에 대해 실행되는 방식으로 체이닝 할 수 있다', () => {
+      var adviceFactory = function(adviceID) {
+        return (function(targetInfo) {
+          executionPoints.push('wrappingAdvice - 처음 ' + adviceID);
+          targetInfo.fn();
+          executionPoints.push('wrappingAdvice - 끝 ' + adviceID);
+        });
+      };
+      Aop.around('targetFn', adviceFactory('안쪽'), targetObj);
+      Aop.around('targetFn', adviceFactory('바깥쪽'), targetObj);
+      targetObj.targetFn();
+      
       expect(executionPoints).toEqual([
         'wrappingAdvice - 처음 바깥쪽',
         'wrappingAdvice - 처음 안쪽',
